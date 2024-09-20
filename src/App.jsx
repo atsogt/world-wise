c6db5a872d6556be7cbeb0b8afe43e876cc6f734
atsogt <atsogt17@gmail.com>
2024-09-20 14:56:43 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import PageNav from "./components/PageNav";
+
+//routes <-- client-side routing
+// /
+// /login
+// /app
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <PageNav />
+      </header>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
