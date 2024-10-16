fa94c8f197cf7b8ba381973cb3d0fddc4edc3bf9
atsogt <atsogt17@gmail.com>
2024-10-15 23:37:08 -0400

    query parameter implemented with lat and lng

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import styles from "./Map.module.css";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Map() {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
   return (
-    <div className={styles.mapContainer}>
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}>
       Map
       <h1>Lat: {lat}</h1>
       <h1>Lng: {lng}</h1>
