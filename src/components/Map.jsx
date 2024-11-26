968f5741c110a51e723d86905c739266f7a8d10a
atsogt <atsogt17@gmail.com>
2024-11-26 11:19:41 -0500

    react-leaflet map implemented and working

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,47 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./Map.module.css"
-import { useState } from "react"
-import { useNavigate, useSearchParams } from "react-router-dom"
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-} from "https://cdn.esm.sh/react-leaflet"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 
-export default function Map() {
-  const navigate = useNavigate()
-  const [searchParams, setSearchParams] = useSearchParams()
-  const [mapPosition, setMapPosition] = useState([50, 20])
-
-  const lat = searchParams.get("lat")
-  const lng = searchParams.get("lng")
+function Map() {
+  const [mapPosition, setMapPosition] = useState([40, 0])
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate("form")
-      }}>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={false}
+        className={styles.mapContainer}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={mapPosition}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
       </MapContainer>
-      Map
-      <h1>Lat: {lat}</h1>
-      <h1>Lng: {lng}</h1>
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 50, lng: 25 })
-        }}>
-        Change pos
-      </button>
     </div>
   )
 }
+
+export default Map
